d6407db9f1f005eae04535885f0c316bacffe6dd
unknown <laidy34x@gmail.com>
2025-03-31 16:58:28 -0300

    nota do aluno e se esta de recuperação

--- exercicio07.py
+++ exercicio07.py
@@ -5,9 +5,13 @@ nota3 = float (input("terceira nota:"))
 
 media = (nota1 + nota1 + nota3)/3
 
-if media <=7: print("aprovado")
-
-else: print("reprovado")
+if media <=4:
+    print( f"reprovado")
+else:
+    if media >= 7:
+        print(f"aprovado{media:.2f}")
+    else:
+        print("recuperaçao")
 
 
 
