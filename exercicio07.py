5dadc595e564fcbff9fce702d9ae12ae56d9bbb4
unknown <laidy34x@gmail.com>
2025-03-31 16:36:30 -0300

    nota do aluno

---
+++ exercicio07.py
@@ -0,0 +1,14 @@
+#nota_do aluno
+nota1 = float (input("primeira nota:"))
+nota2 = float (input("segunda nota:"))
+nota3 = float (input("terceira nota:"))
+
+media = (nota1 + nota1 + nota3)/3
+
+if media <=7: print("aprovado")
+
+else: print("reprovado")
+
+
+
+
--- exercicio6.py
+++ exercicio6.py
@@ -7,5 +7,5 @@ if num1 > num2:
 
 else:
     print(num1,num2)
-    
+
 
