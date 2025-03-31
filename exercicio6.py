228ffc8e43f606a2caf4416fa50f0af985263c9a
unknown <laidy34x@gmail.com>
2025-03-31 16:35:29 -0300

    numero maior ou maior

---
+++ exercicio6.py
@@ -0,0 +1,11 @@
+#Mostre qual numero e maior
+num1 = int (input("digite o numero um:"))
+num2 = int(input("digite o numero dois:"))
+if num1 > num2:
+    print(num2,num1)
+
+
+else:
+    print(num1,num2)
+    
+
